3b71ea3a4d20661c28042dacf6274f7afd6adc62
fddiferd <fddiferd@gmail.com>
2025-05-09 11:15:28 -0700

    removing local wristband

--- backend/wristband/enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class CallbackResultType(Enum):
-    """
-    Enum representing the result type of a callback.
-    """
-    COMPLETED = 'COMPLETED'
-    REDIRECT_REQUIRED = 'REDIRECT_REQUIRED'
